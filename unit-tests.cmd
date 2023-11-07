9d74828622434cfd3629f17bc172935eb47cb132
lborgman <lennart.borgman@gmail.com>
2023-11-07 15:52:17 +0100

    Add unit-tests.cmd/.sh Also call them from package.json

---
+++ unit-tests.cmd
@@ -0,0 +1,4 @@
+@setlocal
+@rem "NODE_OPTIONS=--experimental-vm-modules jest",
+set NODE_OPTIONS=--experimental-vm-modules
+npx jest unit
---
+++ unit-tests.sh
@@ -0,0 +1,4 @@
+# "NODE_OPTIONS=--experimental-vm-modules jest",
+# FIX-ME: syntax for set?
+set NODE_OPTIONS=--experimental-vm-modules
+npx jest unit
