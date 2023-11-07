4839776adabad9252aa56087ac7f574f4483cd84
lborgman <lennart.borgman@gmail.com>
2023-11-07 22:27:32 +0100

    Fixed unit-tests.sh I believe this version works, but I can't test.

--- unit-tests.sh
+++ unit-tests.sh
@@ -1,4 +1,5 @@
 # "NODE_OPTIONS=--experimental-vm-modules jest",
 # FIX-ME: syntax for set?
-set NODE_OPTIONS=--experimental-vm-modules
-npx jest unit
+# set NODE_OPTIONS=--experimental-vm-modules
+# npx jest unit
+npx NODE_OPTIONS=--experimental-vm-modules jest unit
